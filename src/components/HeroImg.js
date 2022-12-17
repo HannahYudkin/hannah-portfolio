b86bcf85458252d2075d14a3a650db858ea106af
HannahYudkin <catgirlblue@gmail.com>
2022-12-16 19:46:50 -0500

    Created hero

---
+++ src/components/HeroImg.js
@@ -0,0 +1,26 @@
+import "./HeroImgStyles.css";
+
+import React from 'react';
+
+import IntroImg from "../assets/meeting.jpg";
+import { Link } from "react-router-dom";
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+      <div className="mask">
+        <img className="intro-img" src={IntroImg} alt="intro-image" />
+      </div>
+      <div className="content">
+        <p> Hi, I'm Max Williams.</p>
+        <h1>Director of Sales Development of the Americas at Sitecore</h1>
+        <div>
+          <Link to="/projects" className="btn">Projects</Link>
+          <Link to="/contact" className="btn btn-light">Contact</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroImg
