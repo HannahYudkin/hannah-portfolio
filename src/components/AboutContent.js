e73d2c7aec43eb80de122632d40463198d5dd5ed
HannahYudkin <catgirlblue@gmail.com>
2022-12-17 21:20:57 -0500

    created/styled AboutContent

---
+++ src/components/AboutContent.js
@@ -0,0 +1,31 @@
+import "./AboutContentStyles.css";
+
+import React from 'react'
+import { Link } from "react-router-dom";
+import TopImage from "../assets/graph.jpg";
+import BottomImage from "../assets/computer.jpg";
+
+
+const AboutContent = () => {
+  return (
+    <div className="about">
+      <div className="left">
+        <h1>Who am I?</h1>
+        <p>I help brands create meaningful experiences that result in customer loyalty & advocacy. I direct the front-end sales process for Sitecore in North America, overseeing both Inbound and Outbound sales development. </p>
+        <Link tp="/contact" className="btn">Contact</Link>
+      </div>
+      <div className="right">
+        <div className="img-container">
+          <div className="img-stack top">
+            <img src={TopImage} className="img" alt="true" />
+          </div>
+          <div className="img-stack bottom">
+            <img src={BottomImage} className="img" alt="true" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutContent
--- src/routes/About.js
+++ src/routes/About.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from "../components/Footer";
 import HeroImg2 from '../components/HeroImg2';
+import AboutContent from '../components/AboutContent';
 
 const About = () => {
   return (
     <div>
       <Navbar />
       <HeroImg2 heading="About me." text="Information about the completely serious, most work-appropriate individual named Max." />
+      <AboutContent />
       <Footer />
     </div>
   )
